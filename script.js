bcd8d109e6d45137299b13841cf39d74eeb81282
eleneesatia13 <154421844+eleneesatia13@users.noreply.github.com>
2024-04-18 13:16:21 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () =>{
     document.querySelector(".menu").addEventListener("click", ()=>{
-        document.querySelector(".nav-bar").classList.add("active");
+        document.querySelector(".nav-bar").classList.add("active")
     })
     document.querySelector(".close").addEventListener("click", ()=>{
-        document.querySelector(".nav-bar").classList.remove("active");
+        document.querySelector(".nav-bar").classList.remove("active")
+    })
+    document.querySelectorAll(".element").forEach((element)=>{
+        element.addEventListener("click", ()=>{
+            document.querySelector(".nav-bar").classList.remove("active")
+        })
     })
 })
